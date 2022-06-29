b377e82050cdeb37a8f499afc90536cc0144367e
HagarMohamed <hagarabdo7@gmail.com>
2022-06-30 00:40:49 +0200

    Create navbar
    1. change sass to scss for all component
    2. create new design for navbar

--- src/app/view/banner/banner.component.ts
+++ src/app/view/banner/banner.component.ts
@@ -4,18 +4,19 @@ import { NewsServiceService } from '../../service/news-service.service';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.sass']
+  styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
 
   constructor(private service: NewsServiceService) { }
 
-  slides = [];
+  slides: any = [];
+  
   ngOnInit(): void {
 
    this.service.getSlides((response: any) => {
     console.log(response);
-    this.slides = response;
+    this.slides = response.slides;
    })
 
   }
